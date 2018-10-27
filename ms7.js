64e8b81d1c905f9511e4ad1bf8b7e2d400247b1f
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2018-10-27 16:38:09 +0200

    Update ms7.js

--- ms7.js
+++ ms7.js
@@ -1,23 +1,23 @@
 const fs = require ("fs")
-const Discord  = require('discord.js'),
-			client   = new Discord.Client()
+const Discord  = require('discord.js'), //
+            client   = new Discord.Client()
 
 client.on('ready', () => {
-	console.log('I am ready!');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-	if (message.author.bot) return;
-	if (message.author.id == 224875948372262912) {
-		if (message.content === 'مسح') {
-			message.channel.fetchMessages().then(messages => messages.forEach(msg => msg.delete()));
-		} else if (message.content.match(/\/clear \d+/)) {
-			var limit = message.content.match(/\/clear (\d+)/)[1] + 1;
-			message.channel.fetchMessages({limit: limit}).then(messages => messages.forEach(msg => msg.delete()));
-		}
-	} else {
+    if (message.author.bot) return;
+    if (message.author.id == 224875948372262912) {
+        if (message.content === 'مسح') {
+            message.channel.fetchMessages().then(messages => messages.forEach(msg => msg.delete()));
+        } else if (message.content.match(//clear \d+/)) {
+            var limit = message.content.match(//clear (\d+)/)[1] + 1;
+            message.channel.fetchMessages({limit: limit}).then(messages => messages.forEach(msg => msg.delete()));
+        }
+    } else {
 
-	}
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
