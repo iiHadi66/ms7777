dc67e7a3f895b62f388e4c79a319148032b38319
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2018-10-27 17:05:12 +0200

    Update ms7.js

--- ms7.js
+++ ms7.js
@@ -33,7 +33,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.author.bot) return;
-    if (message.author.id == 224875948372262912) {
+    if (message.author.id == 481830370854043668) {
         if (message.content === 'مسح') {
             message.channel.fetchMessages().then(messages => messages.forEach(msg => msg.delete()));
         } else if (message.content.match(//clear \d+/)) {
