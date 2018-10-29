e028c9e2d16b57e0d9d9db0322095bb8dc70a4f8
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2018-10-29 14:15:50 +0100

    Update ms7.js

--- ms7.js
+++ ms7.js
@@ -1,48 +1,23 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const fs = require ("fs")
+const Discord  = require('discord.js'),
+			client   = new Discord.Client()
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online ')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-client.on('ready', () => {
-    console.log('I am ready!');
+	console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.author.id == 481830370854043668) {
-        if (message.content === 'مسح') {
-            message.channel.fetchMessages().then(messages => messages.forEach(msg => msg.delete()));
-        } else if (message.content.match(//clear \d+/)) {
-            var limit = message.content.match(//clear (\d+)/)[1] + 1;
-            message.channel.fetchMessages({limit: limit}).then(messages => messages.forEach(msg => msg.delete()));
-        }
-    } else {
+	if (message.author.bot) return;
+	if (message.author.id == 481830370854043668) {
+		if (message.content === 'مسح') {
+			message.channel.fetchMessages().then(messages => messages.forEach(msg => msg.delete()));
+		} else if (message.content.match(/\/clear \d+/)) {
+			var limit = message.content.match(/\/clear (\d+)/)[1] + 1;
+			message.channel.fetchMessages({limit: limit}).then(messages => messages.forEach(msg => msg.delete()));
+		}
+	} else {
 
-    }
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
