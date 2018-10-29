fa6207199c3b3f4314256953565e64febbc39bc6
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2018-10-29 14:09:49 +0100

    Update ms7.js

--- ms7.js
+++ ms7.js
@@ -1,6 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const fs = require ("fs")
-const Discord  = require('discord.js'),
-            client   = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online ')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -11,8 +37,8 @@ client.on('message', message => {
     if (message.author.id == 481830370854043668) {
         if (message.content === 'مسح') {
             message.channel.fetchMessages().then(messages => messages.forEach(msg => msg.delete()));
-        } else if (message.content.match(/\/clear \d+/)) {
-            var limit = message.content.match(/\/clear (\d+)/)[1] + 1;
+        } else if (message.content.match(//clear \d+/)) {
+            var limit = message.content.match(//clear (\d+)/)[1] + 1;
             message.channel.fetchMessages({limit: limit}).then(messages => messages.forEach(msg => msg.delete()));
         }
     } else {
@@ -21,3 +47,4 @@ client.on('message', message => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
