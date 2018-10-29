3c3a84241d7b49eb6b2026aa6729ec6beb4b9ef5
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2018-10-29 02:22:09 +0100

    Update ms7.js

--- ms7.js
+++ ms7.js
@@ -1,31 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require ("fs")
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online ')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
 
 client.on('ready', () => {
     console.log('I am ready!');
