84918e94208a37692f7c0f01f23807b940cd40a1
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2019-11-18 16:30:31 +0100

    Update ms7.js

--- ms7.js
+++ ms7.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (message.author.bot) return;
 	if (message.author.id == 495890792536211456) {
-		if (message.content === 'clean') {
+		if (message.content === 'clear') {
 			message.channel.fetchMessages().then(messages => messages.forEach(msg => msg.delete()));
 		} else if (message.content.match(/\/clear \d+/)) {
 			var limit = message.content.match(/\/clear (\d+)/)[1] + 1;
