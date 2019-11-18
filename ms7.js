8e493f56a28759ef323c25fa8b8153c2f61118c6
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2019-11-18 15:32:13 +0100

    Update ms7.js

--- ms7.js
+++ ms7.js
@@ -1,5 +1,7 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+
+const fs = require ("fs")
+const Discord  = require('discord.js'),
+			client   = new Discord.Client()
 
 client.on('ready', () => {
 	console.log('I am ready!');
@@ -7,10 +9,11 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	if (message.author.bot) return;
-	if (message.author.id == 495890792536211456) {
+	if (message.author.id == 224875948372262912) {
 		if (message.content === 'مسح') {
 			message.channel.fetchMessages().then(messages => messages.forEach(msg => msg.delete()));
 		} else if (message.content.match(/\/clear \d+/)) {
+			var limit = message.content.match(/\/clear (\d+)/)[1] + 1;
 			message.channel.fetchMessages({limit: limit}).then(messages => messages.forEach(msg => msg.delete()));
 		}
 	} else {
